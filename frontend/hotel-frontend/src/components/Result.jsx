569e7fefe4f8227825d4df7b19d02aa865dc984a
gthsun <sundaram.geethan@gmail.com>
2025-03-29 18:24:39 -0400

    Improved styling

--- frontend/hotel-frontend/src/components/Result.jsx
+++ frontend/hotel-frontend/src/components/Result.jsx
@@ -3,15 +3,27 @@ import "../styles/Results.css";
 function Results() {
   return (
     <div className="result-box">
-      <h4>Grand Central Hotel</h4>
-      <h5>230 E 51st St, New York, NY 10022</h5>
-      <div className="match-score">Match: 92%</div>
-      <ul className="description-list">
-        <li>Walkable to 5th Avenue</li>
-        <li>Can fit 4 people</li>
-        <li>Low noise area</li>
-        <li>Pet friendly</li>
-      </ul>
+      <div className="result-left">
+        <h3>Grand Central Hotel</h3>
+        <h5>230 E 51st St, New York, NY 10022</h5>
+        <div className="match-score">Match: 92%</div>
+      </div>
+      <div className="result-right">
+        <h4>What matches?</h4>
+        <ul className="description-list">
+          <li>Walkable to 5th Avenue</li>
+          <li>Can fit 4 people</li>
+          <li>Low noise area, where sleep disturbance is unlikely</li>
+          <li>Pet friendly</li>
+        </ul>
+        <h4>What doesn't match?</h4>
+        <ul className="description-list">
+          <li>Walkable to 5th Avenue</li>
+          <li>Can fit 4 people</li>
+          <li>Low noise area, where sleep disturbance is unlikely</li>
+          <li>Pet friendly</li>
+        </ul>
+      </div>
     </div>
   );
 }
