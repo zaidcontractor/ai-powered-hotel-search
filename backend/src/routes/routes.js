f73d0b8470ee6a4c348862977e9b6233b80bfc8b
Daksh <ghadiadaksh@gmail.com>
2025-03-29 15:11:37 -0400

    Fixed issues in apiservice

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,12 +1,15 @@
 // server/routes/dataRoutes.js
 import { Router } from 'express';
+import apiservice from "../services/externalApiService.js";
 // import { fetchData } from '../controllers/dataController';
 
 const router = Router();
 
 // Define a GET endpoint for retrieving the external API data
 router.get('/', async(req,res) => {
-    res.send("Hi")
+    res.send("Hi");
+    
+    
 });
 
 export default router;
--- backend/src/services/externalApiService.js
+++ backend/src/services/externalApiService.js
@@ -1,5 +1,5 @@
 // server/services/externalApiService.js
-import { get } from 'axios';
+import axios from 'axios';
 
 // Optionally, define your base URL
 const BASE_URL = 'https://api.example.com'; // replace with the actual API base URL
@@ -8,7 +8,7 @@ const API_KEY = process.env.API_KEY;
 async function getSpecificData(params) {
   try {
     // Append your API key to the request parameters
-    const response = await get(`${BASE_URL}/desired-endpoint`, {
+    const response = await axios.get(`${BASE_URL}/desired-endpoint`, {
       params: { ...params, apiKey: API_KEY },
     });
     return response.data;
