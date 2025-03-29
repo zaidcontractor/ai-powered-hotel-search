043cef9be40142ad989e0b0f272292fc0c28784d
gthsun <sundaram.geethan@gmail.com>
2025-03-29 14:49:44 -0400

    Added more styling

---
+++ frontend/hotel-frontend/src/Home.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./Home.css";
+
+function App() {
+  const [prompt, setPrompt] = useState('');
+
+  return (
+    <>
+      <div className="main-flex">
+        <div className="main">
+          <h1>AI Powered Hotel Search</h1>
+          <br></br>
+          <textarea
+            className="prompt"
+            name="prompt"
+            placeholder="Enter your prompt"
+            required
+          ></textarea>
+          <br></br>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
