822a75f2c861e3d5fb022d73ab7d35f631b894e0
Zaid Contractor <zaidcontractor05@gmail.com>
2025-03-30 02:30:55 -0400

    Modified paths and commented out database-related code in server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
-const connectDB = require('./config/db');
+// const connectDB = require('./config/db');
 
 const app = express();
 
@@ -21,5 +21,5 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
