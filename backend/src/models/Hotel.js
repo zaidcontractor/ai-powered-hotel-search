af1cb984e047a3e6d5cc9801421b9dc4652757be
gthsun <sundaram.geethan@gmail.com>
2025-03-30 04:20:32 -0400

    Defined mongoDB schema for hotel result

--- backend/src/models/Hotel.js
+++ backend/src/models/Hotel.js
@@ -1,9 +1,56 @@
 import mongoose from "mongoose";
 
-const hotelSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  location: String,
-  price: Number,
-});
+const { Schema } = mongoose;
 
-export default mongoose.model("Hotel", hotelSchema);
+const hotelSchema = new Schema(
+  {
+    chainCode: {
+      type: String,
+      required: true,
+    },
+    iataCode: {
+      type: String,
+      required: true,
+    },
+    dupeId: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    hotelId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    geoCode: {
+      latitude: {
+        type: Number,
+        required: true,
+      },
+      longitude: {
+        type: Number,
+        required: true,
+      },
+    },
+    address: {
+      countryCode: {
+        type: String,
+        required: true,
+      },
+    },
+    lastUpdate: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Hotel = mongoose.model("Hotel", hotelSchema);
+
+export default Hotel;
